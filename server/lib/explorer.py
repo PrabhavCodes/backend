e4f26d12af2e585eeab6c7ac0a10d989df838c33
Lakshdeep Singh <lakshdeep.code@gmail.com>
2025-03-07 18:43:02 +0530

    Fixed explorer

--- server/lib/explorer.py
+++ server/lib/explorer.py
@@ -7,7 +7,11 @@ from langchain_core.tools import tool
     description="Useful for retreiving relevant pages from Wikipedia",
     response_format="content"
 )
-def explore(query: str, max_results: int = 2, load_all_available_meta: bool = True, doc_chars_max: int = 8000):
-    wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper(top_k_results=max_results, load_all_available_meta=load_all_available_meta, doc_content_chars_max=doc_chars_max))
+def explore(query: str):
+    wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper(
+        top_k_results=3,
+        load_all_available_meta=True,
+        doc_content_chars_max=6000
+    ))
     return wikipedia.run(query).split("\n\n")
 
